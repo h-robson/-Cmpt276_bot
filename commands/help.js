e3d379189881e934f3c5dd359064a3283bc57369
Hannah Robson <hcr2@sfu.ca>
2021-07-23 14:54:35 -0700

    Use process env instead of json take 2

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const { prefix } = require('../../config.json');
+/*const { prefix } = require('../../config.json');
 module.exports = {
 	name: 'help',
 	description: 'List commands and info.',
@@ -12,4 +12,4 @@ module.exports = {
 			
 		}
 	},
-};
+};*/
