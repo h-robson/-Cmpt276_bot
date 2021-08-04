4a9a5202f25f7647f94ce851a690f20b4c898bd2
Sunderinq <tegnoor.gill@hotmail.com>
2021-08-03 17:41:44 -0700

    env deleted off of githib

--- index.js
+++ index.js
@@ -1,52 +1,6 @@
-require('dotenv').config();
-const fs = require('fs');
-const Discord = require('discord.js');
-//const { prefix, token } = require('./config.json');
-const client = new Discord.Client();
+require("dotenv").config();
 
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const { Client } = require('discord.js');
+const client = new Client();
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-const prefix = process.env.PREFIX;
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	if (!client.commands.has(commandName)) return;
-
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	//checks if command require arguments and if any are provided
-	if (command.args && !args.length) {
-		let reply = `You didn't provide an argument, ${message.author}!`;
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-		return message.channel.send(reply);
-	}
-
-	try {
-		command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('Error executing command');
-	}
-
-	console.log(message.content);
-});
-
-client.login(process.env.DIS_TOKEN);
+client.login()
